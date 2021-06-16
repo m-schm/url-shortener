1e13fe42a6d31c442d77a926b098747e56d2f1fa
m-schm <55513173+m-schm@users.noreply.github.com>
2021-06-15 19:11:07 -0700

    thread the sql backend through everything

--- src/Shortener/DB.hs
+++ src/Shortener/DB.hs
@@ -12,14 +12,15 @@ import Database.Persist.Postgresql
 import Control.Monad.Except (ExceptT(..), lift)
 
 runPostgresHandler :: BackendCompatible SqlBackend backend =>
-  backend -> PostgresHandler a -> Handler a
+  backend -> PostgresHandler backend a -> Handler a
 runPostgresHandler backend (PostgresHandler ma) =
-  Handler . ExceptT . flip runSqlConn backend . lift . pure $ ma
+  Handler . ExceptT . flip runSqlConn backend . runExceptT $ ma
 
-newtype PostgresHandler a = PostgresHandler (Either ServerError a)
+newtype PostgresHandler backend a =
+  PostgresHandler (ExceptT ServerError (ReaderT backend IO) a)
   deriving newtype
     ( Functor, Applicative, Monad
-    , MonadError ServerError
+    , MonadError ServerError, MonadIO
     )
 
 instance MonadShortener PostgresHandler where
