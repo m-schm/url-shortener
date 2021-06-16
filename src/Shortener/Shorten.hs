a44cd53a2620ba2ae37f948f970b3e8d9d66a67b
m-schm <55513173+m-schm@users.noreply.github.com>
2021-06-15 19:03:49 -0700

    fix docs and simplify url generation algorithm

--- src/Shortener/Shorten.hs
+++ src/Shortener/Shorten.hs
@@ -14,18 +14,16 @@ import Shortener.API
 -- | Creates an infinite list of 'ShortId's from a 'FullUrl' from its hash.
 -- Deterministic shortening ensures that when shortening the same URL twice,
 -- a pre-used row in the DB can be returned instead of creating multiple
--- unnecessary duplicates
+-- unnecessary duplicates, while returning a stream of results ensures
+-- 'ShortId's can still be generated in the face of hash collisions
 createShortIds :: FullUrl -> [ShortId]
 createShortIds (FullUrl t) =
   let hash :: Digest SHA256
       hash = C.hash (encodeUtf8 t)
 
-      options :: [Integer]
-      options = fmap fromBase256 $ tails (unpack hash)
-
-  in fmap (iterate (1 +)) options
-     &   concat . transpose
-     <&> ShortId . T.pack . fmap (base64 !!) . toBase64
+  in  fromBase256 (unpack hash)
+  &   iterate (1 +)
+  <&> ShortId . T.pack . take shortIdLen . fmap (base64 !!) . toBase64
 
 -- | Interprets a list of 'Word8's as an integer in base-256, with least
 -- sigificant digit on the right
@@ -33,14 +31,25 @@ fromBase256 :: [Word8] -> Integer
 fromBase256 = foldl' (\acc digit -> acc*256 + fromIntegral digit) 0
 
 -- | Converts an 'Integer' to a list of base-64 digits, with least significant
--- digit on the right
+-- digit on the left
 toBase64 :: Integer -> [Int]
-toBase64 = go [] where
-  go acc 0 = acc
-  go acc n =
-    let (d, m) = n `divMod` 64
-    in go (fromIntegral m : acc) d
+toBase64 0 = []
+toBase64 n =
+  let (d, m) = n `divMod` 64
+  in fromIntegral m : toBase64 d
 
 -- | Youtube's web-safe base64 alphabet
+--
+-- @
+-- base64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_"
+-- @
 base64 :: [Char]
 base64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_"
+
+-- | Length (in characters) of generated 'ShortId's
+--
+-- @
+-- shortIdLen = 6
+-- @
+shortIdLen :: Int
+shortIdLen = 6
